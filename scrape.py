ba51c835273369f46c122ff5263b2a8ab4cdd95a
Chad Boyce <chadboyce@gmail.com>
2020-02-22 20:22:57 -0500

    got scrape.py tutorial working so far

--- scrape.py
+++ scrape.py
@@ -1,5 +1,14 @@
 import requests
 import lxml.html
 
-html = requests.get("https://www.beatport.com/genre/psy-trance/13/top-100")
+# html = requests.get("https://www.beatport.com/genre/psy-trance/13/top-100")
+html = requests.get("https://store.steampowered.com/explore/new/")
 doc = lxml.html.fromstring(html.content)
+
+new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')
+doc = lxml.html.fromstring(html.content)
+print(new_releases)
+
+new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
+titles = new_releases.xpath('//div[@class="tab_item_name"]/text()')
+print(titles)
