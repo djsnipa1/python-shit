e3614dafe0d944e1febec0fc070917693c5ede93
Chad Boyce <chadboyce@gmail.com>
2020-03-03 16:13:14 -0500

    pretty much got get_title.py working! and added a few PEP8 changes

--- get_title.py
+++ get_title.py
@@ -26,26 +26,26 @@ s = 'This is my tweet check it out http://tinyurl.com/blah and http://blabla.com
 print(re.findall(r'(https?://\S+)', s))
 
 with open('parse.txt', 'r', encoding="utf8") as fh:
-  data = fh.read().replace('\n', '')
-  # print(data)
-  # links = re.findall(r'(https?://\S+)', data)
-  # print(links)
+    data = fh.read().replace('\n', '')
+    # print(data)
+    # links = re.findall(r'(https?://\S+)', data)
+    # print(links)
 
-  extractor = URLExtract()
-  urls = extractor.find_urls(data)
-  # print(urls)  # prints: ['janlipovsky.cz']
-  # print(type(urls))
+    extractor = URLExtract()
+    urls = extractor.find_urls(data)
+    # print(urls)  # prints: ['janlipovsky.cz']
+    # print(type(urls))
 
-  for url in urls:
-    html = requests.get(url)
-    doc = lxml.html.fromstring(html.content)
-    title = doc.find(".//title").text
-    description = doc.xpath('.//meta[@name="description"]/@content')
-    # print(url)
-    # print(title)
-    # print(description)
-    # print("---------------------------------")
-    print("\n[" + title + "](" + url + ")")  # adds new line in front of markdown links
-    split = str(description)[2:-2]  # Removes the [ ] and ' ' from each description line
-    print(split + '\n')  # adds a new line after the description
-    print("- - -")  # obviously adds a hr to the file
+    for url in urls:
+        html = requests.get(url)
+        doc = lxml.html.fromstring(html.content)
+        title = doc.find(".//title").text
+        description = doc.xpath('.//meta[@name="description"]/@content')
+        # print(url)
+        # print(title)
+        # print(description)
+        # print("---------------------------------")
+        print("\n[" + title + "](" + url + ")")  # adds new line in front of markdown links
+        split = str(description)[2:-2]  # Removes the [ ] and ' ' from each description line
+        print(split + '\n')  # adds a new line after the description
+        print("- - -")  # obviously adds a hr to the file
