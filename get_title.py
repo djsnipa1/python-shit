81452a81212fa5cef8db3cccf5b2f5a3cf76441e
Chad Boyce <chadboyce@gmail.com>
2020-03-02 20:40:37 -0500

    modified get_title.py

--- get_title.py
+++ get_title.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+the web url matching regex used by markdown
+http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+https://gist.github.com/gruber/8891611
+"""
+
 import lxml.html
 # from urllib2 import urlopen
 import requests
+import re
 
 
 html = requests.get("https://store.steampowered.com/explore/new/")
@@ -22,3 +31,14 @@ for url in urls:
   print(title)
   print(description)
   print("---------------------------------")
+
+s = 'This is my tweet check it out http://tinyurl.com/blah and http://blabla.com'
+print(re.findall(r'(https?://\S+)', s))
+
+with open('parse.txt', 'r', encoding="utf8") as fh:
+  data = fh.read().replace('\n', '')
+  print(data)
+  # all_lines = fh.readlines()
+  # print(all_lines)
+  # links = re.findall(r'(https?://\S+)', all_lines)
+  # print(links)
