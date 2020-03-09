149b3a014f8ccdf372a3f14b20cb073741cdae97
Chad Boyce <chadboyce@gmail.com>
2020-03-09 12:13:02 -0400

    trying to work on lassie_test.py

---
+++ lassie_test.py
@@ -0,0 +1,24 @@
+import lxml.html
+from bs4 import BeautifulSoup
+# from urllib2 import urlopen
+import requests
+import html2text
+import re
+from urlextract import URLExtract
+
+html = requests.get("https://www.dataquest.io/blog/web-scraping-tutorial-python/")
+# html = open(response).read()
+# doc = lxml.html.fromstring(html.content)
+
+# soup = BeautifulSoup(html)
+soup = BeautifulSoup(html.content, 'html.parser')
+
+# print(soup.prettify()
+body = soup.find("body")
+pretty = body.prettify()
+print(html2text.html2text(pretty))
+
+with open('lassie_test.md', 'w', encoding='utf-8') as outfile:
+    html2text.html2text(pretty, outfile)
+
+# print(soup.encode("utf-8")
